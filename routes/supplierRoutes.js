2e94977c152fe7de6de955e9340e420066e5d98f
WilliamCucJR <wcuc96dev@gmail.com>
2024-08-25 14:23:40 -0600

    Modificacion en nombre de archivo importado

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supplierController = require('../controllers/supplierController');
+const supplierController = require('../controllers/SupplierController');
 
 router.get('/suppliers', supplierController.getAllSuppliers);
 router.get('/suppliers/:id', supplierController.getSupplierById);
