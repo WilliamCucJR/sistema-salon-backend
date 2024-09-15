8de61e951f187863c1590acae67280b970bb5d26
WilliamCucJR <wcuc96dev@gmail.com>
2024-09-14 22:43:30 -0600

    Integración de jwt en servicio de login

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -2,10 +2,10 @@ const LoginService = require('../services/LoginService.js');
 const loginService = new LoginService();
 
 exports.login = async (req, res) => {
-    try {
-        const user = await loginService.login(req.body);
-        res.json(user);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const user = await loginService.login(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ error: err.message });
+  }
 };
--- services/LoginService.js
+++ services/LoginService.js
@@ -1,19 +1,32 @@
 const Database = require("../data/Database");
+const jwt = require("jsonwebtoken");
 
 class LoginService {
   constructor() {
     this.db = new Database();
+    this.secretKey = process.env.SECRET_KEY;
   }
 
   async login(data) {
-    console.log(data);
-    
     const sql = `SELECT * FROM SDB_USER WHERE USE_USER = ? AND USE_PASSWORD = ?`;
     const values = [data.USE_USER, data.USE_PASSWORD];
     try {
-      return await this.db.query(sql, values);
+      const user = await this.db.query(sql, values);
+      if (user.length > 0) {
+        const token = jwt.sign({ id: user[0].USE_ID, username: user[0].USE_USER }, this.secretKey, {
+          expiresIn: "1h",
+        });
+        return { token };
+      } else {
+        const error = new Error("Credenciales inválidas");
+        error.statusCode = 401;
+        throw error;
+      }
     } catch (err) {
-      throw new Error("Error al obtener el usuario: " + err.message);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      throw err;
     }
   }
 }
