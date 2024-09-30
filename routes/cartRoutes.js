5143910f52e9ce8bbbd424ae2e9291a36a981298
WilliamCucJR <wcuc96dev@gmail.com>
2024-09-29 22:22:20 -0600

    Servicios de carrito de compras y pagos

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cartController = require('../controllers/CartController');
+const cartController = require("../controllers/CartController");
 
-router.get('/cart/:id', cartController.getCartById);
-router.post('/cart', cartController.addToCart);
-router.get('/cart/total/:id', cartController.getTotalByCustomerId);
+router.get("/cart/:id", cartController.getCartById);
+router.post("/cart", cartController.addToCart);
+router.get("/cart/total/:id", cartController.getTotalByCustomerId);
+router.put("/cart/:id", cartController.updateCart);
 
 module.exports = router;
